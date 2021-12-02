6afc1cf756114693d5b67e722d35504244d15c65
g.bulat <mail.grzegorz.bulat@gmail.com>
2021-12-02 20:32:30 +0100

    second

--- 1/Program.fs
+++ 1/Program.fs
@@ -2,21 +2,14 @@
 open System.IO
 open System.Diagnostics
 
-[<EntryPoint>]
-let main(args) = 
-    let getWorkingDirectory = Path.Combine(System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), "input.txt")
-    let p a x y = if x < y then a + 1 else a
-    let ft (x, _, _) = x
-    let sumElements x = List.reduce(fun ac y -> ac + y) x
-    
-    let countBiggerThenPrevious l = List.fold (fun (acc, pr) x -> (p acc pr x, x)) (0, Int32.MaxValue) l |> fst |> printf "%d\n"
+let getInputFile = Path.Combine(System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), "input.txt")
+let p a x y = if x < y then a + 1 else a
+let ft (x, _, _) = x
+let sumElements x = List.reduce(fun ac y -> ac + y) x
 
-    let parsedInput = File.ReadLines(getWorkingDirectory) |> 
-                        List.ofSeq |>
-                        List.map int
-
-    countBiggerThenPrevious parsedInput     
-
-    parsedInput |> List.windowed 3 |> List.map(fun x -> sumElements x) |> countBiggerThenPrevious
-    
-    0
+let countBiggerThenPrevious l = List.fold (fun (acc, pr) x -> (p acc pr x, x)) (0, Int32.MaxValue) l |> fst |> printf "%d\n"
+let parsedInput = File.ReadLines(getInputFile) |> 
+                    List.ofSeq |>
+                    List.map int
+countBiggerThenPrevious parsedInput     
+parsedInput |> List.windowed 3 |> List.map(fun x -> sumElements x) |> countBiggerThenPrevious
---
+++ 2/Program.fs
@@ -0,0 +1,31 @@
+﻿open System
+open System.IO
+open System.Diagnostics
+
+let move1 (x,y) instruction n = 
+    match instruction with
+    | "forward" -> (x + n, y)
+    | "up"      -> (x, y - n)
+    | "down"    -> (x, y + n)
+    | _ -> raise (ArgumentException("Not supported instruction"))
+
+let move2 (x,y,aim) instruction n = 
+    match instruction with
+    | "forward" -> (x + n, y + aim * n, aim)
+    | "up"      -> (x, y, aim - n)
+    | "down"    -> (x, y, aim + n)
+    | _ -> raise (ArgumentException("Not supported instruction"))
+
+let mulT (x, y) = x*y
+let mulT3 (x, y, _) = x*y
+
+let getInputFile = Path.Combine(System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), "input.txt")
+
+let instructions = File.ReadLines(getInputFile) |> 
+                    List.ofSeq |>
+                    List.map (fun x -> x.Split [|' '|]) |>
+                    List.map (fun x -> (x[0], int x[1]))
+
+instructions |> List.fold (fun pos (i, n) -> move1 pos i n) (0, 0) |> mulT |> printf "%d"
+
+instructions |> List.fold (fun pos (i, n) -> move2 pos i n) (0, 0, 0) |> mulT3 |> printf "%d"
